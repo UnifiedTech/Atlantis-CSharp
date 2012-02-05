1d12fce5432e0dd1130eac8d253a37a2530b1e4d
Zack Loveless <bhmath@gmail.com>
2012-02-04 20:46:22 -0700

    -

---
+++ trunk/Atlantis.TestApp/Speech/RandomTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atlantis.TestApp.Speech
+{
+    class RandomTest
+    {
+        public static void Main()
+        {
+            const Int32 max = 25;
+            Int32 last = 0;
+            for (Int32 i = 0; i < max; ++i)
+            {
+                Int32 seed = new Random().Next(Convert.ToInt32(DateTime.MinValue), Convert.ToInt32(DateTime.MaxValue));
+
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ trunk/UnitTests/Generic/RandomNumbers.cs
@@ -0,0 +1,30 @@
+﻿namespace UnitTests.Generic
+{
+    using Atlantis.Linq;
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RandomNumbers
+    {
+        [TestMethod]
+        public void TwentyFiveRandomNumbers()
+        {
+            const Int32 MAX_COUNT = 25;
+
+            List<Int32> randomInts = new List<Int32>();
+            Int32 seed = Convert.ToInt32(DateTime.MinValue.ToTimestamp());
+            for (Int32 i = 0; i < MAX_COUNT; ++i)
+            {
+                Int32 x = new Random(seed).Next(0, Convert.ToInt32(DateTime.MinValue.ToTimestamp()));
+                Assert.IsFalse(randomInts.Contains(x));
+                randomInts.Add(x);
+
+                seed = new Random().Next(Convert.ToInt32(DateTime.MinValue.ToTimestamp()), Convert.ToInt32(DateTime.MaxValue.ToTimestamp()));
+            }
+        }
+    }
+}
